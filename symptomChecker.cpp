127dffea0ad10835f8fef87fd32b22a28f9f0070
NadaSleep <tyler.m.sexton@gmail.com>
2024-04-14 16:34:08 -0400

    Added Table class that will take care of the graphics.

---
+++ symptomChecker.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std; 
+
+namespace Graphics {
+  class Table  {
+    void displayTable () {
+      cout <<  
+
+  }
+ }
+}
+
+
+
+int main(void)
+{
+cout << ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
