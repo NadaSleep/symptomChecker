a367541fc31351434106eb07f16f53c6ad7d8cca
NadaSleep <tyler.m.sexton@gmail.com>
2024-04-14 19:26:09 -0400

    Finished table, displaying correctly.

--- symptomChecker.cpp
+++ symptomChecker.cpp
@@ -1,37 +1,33 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std; 
 
 namespace Graphics {
   class Table  {
+  public:  
     void displayTable () {
-      cout <<  
-
+      cout <<"Guide:       * Common    + Sometimes/Rarely    - NO"<< endl
+           <<"======================================================="<< endl
+           <<"Symptoms                |  COVID-19  |  Cold  |  Flu  |"<< endl
+           <<"======================================================="<< endl
+           <<"Fever                   |      *     |    +   |   *   |"<< endl
+           <<"Cough                   |      *     |    +   |   *   |"<< endl
+           <<"Shortness of Breath     |      *     |    -   |   -   |"<< endl
+           <<"Runny Nose              |      +     |    *   |   +   |"<< endl
+           <<"Headaches               |      +     |    +   |   *   |"<< endl
+           <<"Sneezing                |      -     |    *   |   -   |"<< endl
+           <<"Fatigue                 |      +     |    +   |   *   |"<< endl
+           <<"======================================================="<< endl;
   }
- }
+ };
 }
 
 
 
 int main(void)
 {
-cout << ""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Graphics::Table newTable;
+newTable.displayTable();
 
 
 
