0470a1d0b86cf1309975783a9845eba0c0d69bc7
NadaSleep <tyler.m.sexton@gmail.com>
2024-04-15 19:04:02 -0400

    Added a method to generate symptoms

--- symptomChecker.cpp
+++ symptomChecker.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <cstdlib>
 
 using namespace std; 
 
@@ -24,40 +25,51 @@ namespace Graphics {
 }
 
 namespace Program {
-  class SymptomChecker {
+  class SymptomChecker {  
   public:
-    int symptomGenerator(int patients) {
-      for (int i = 0; i < patients; i++)
-      int 
-      
-
-
-
-
+  int covid19 = 0, cold = 0, flu  = 0;   
+    void symptomGenerator(int patients) {
+      srand(time(nullptr));
+      for (int i = 0; i < patients; i++) {
+        int fever = rand() % 2;
+        int cough = rand() % 2;
+        int shortnessOfBreath = rand() % 2;
+        int runnyNose = rand() % 2;
+        int headaches = rand() % 2;
+        int sneezing = rand() % 2;
+        int fatigue = rand() % 2;
+//Covid-19 check          
+          if(fever == 1 && cough == 1 && shortnessOfBreath == 1 && sneezing == 0) {
+            covid19++;
+          }   
+//Cold check
+          if(shortnessOfBreath == 0 && runnyNose == 1 && sneezing == 1) {
+            cold++;
+          }
+//Flu check
+          if(fever == 1 && cough == 1 && shortnessOfBreath == 0 && headaches == 1 && sneezing == 0 && fatigue == 1) {
+            flu++;
+          }
+   }      
   }     
  };  
 }
 
-
-
-
-
-
-
-
-
-
-
-
-}
-
-
 int main(void)
 {
+int covid19, cold, flu, patients;
 Graphics::Table newTable;
 newTable.displayTable();
 
+cout << "patient number?" << endl;
+cin >> patients;
 
+Program::SymptomChecker newSymptomChecker;
+newSymptomChecker.symptomGenerator(patients);
+
+cout << covid19 << endl;
+cout << cold << endl;
+cout << flu << endl;
 
 
 
