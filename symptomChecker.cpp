293ed2939914ae8e7227d112557eda3892732faa
NadaSleep <tyler.m.sexton@gmail.com>
2024-04-15 16:10:12 -0400

    created a class for SymptomChecker

--- symptomChecker.cpp
+++ symptomChecker.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std; 
 
@@ -7,21 +8,48 @@ namespace Graphics {
   public:  
     void displayTable () {
       cout <<"Guide:       * Common    + Sometimes/Rarely    - NO"<< endl
-           <<"======================================================="<< endl
-           <<"Symptoms                |  COVID-19  |  Cold  |  Flu  |"<< endl
-           <<"======================================================="<< endl
-           <<"Fever                   |      *     |    +   |   *   |"<< endl
-           <<"Cough                   |      *     |    +   |   *   |"<< endl
-           <<"Shortness of Breath     |      *     |    -   |   -   |"<< endl
-           <<"Runny Nose              |      +     |    *   |   +   |"<< endl
-           <<"Headaches               |      +     |    +   |   *   |"<< endl
-           <<"Sneezing                |      -     |    *   |   -   |"<< endl
-           <<"Fatigue                 |      +     |    +   |   *   |"<< endl
-           <<"======================================================="<< endl;
+           <<"========================================================"<< endl
+           <<"|Symptoms                |  COVID-19  |  Cold  |  Flu  |"<< endl
+           <<"|======================================================="<< endl
+           <<"|Fever                   |      *     |    +   |   *   |"<< endl
+           <<"|Cough                   |      *     |    +   |   *   |"<< endl
+           <<"|Shortness of Breath     |      *     |    -   |   -   |"<< endl
+           <<"|Runny Nose              |      +     |    *   |   +   |"<< endl
+           <<"|Headaches               |      +     |    +   |   *   |"<< endl
+           <<"|Sneezing                |      -     |    *   |   -   |"<< endl
+           <<"|Fatigue                 |      +     |    +   |   *   |"<< endl
+           <<"========================================================"<< endl;
   }
  };
 }
 
+namespace Program {
+  class SymptomChecker {
+  public:
+    int symptomGenerator(int patients) {
+      for (int i = 0; i < patients; i++)
+      int 
+      
+
+
+
+
+  }     
+ };  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 
 int main(void)
